742e2eafde86c64e073e321f2473ce8f260fdb11
Alexey Sofiychuk <divan4ik42@MacBook-Pro-Alexey.local>
2020-07-13 01:23:58 +0300

    test
    test desc

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+        print("Hello my name is Alex");
+        print("Nice to meet You");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
